9c6eab1f551d64dedae276955e1999ad6a874ade
CodeJesterND <dev.hyoseung@gmail.com>
2024-11-12 14:56:43 +0900

    ✅ 테스트 추가 : 유저 생성, 수정, 로그인, 이메일 인증에 대한 유닛 테스트 추가 - UserCreateDto를 사용한 유저 생성 테스트 - UserUpdateDto를 사용한 유저 수정 테스트 - 유저 로그인 시 마지막 로그인 시간 변경 테스트 - PENDING 상태의 유저 이메일 인증을 통한 활성화 테스트 - 잘못된 인증 코드 입력 시 예외 발생 테스트

--- src/test/java/com/example/demo/service/UserServiceTest.java
+++ src/test/java/com/example/demo/service/UserServiceTest.java
@@ -2,17 +2,28 @@ package com.example.demo.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 
+import com.example.demo.exception.CertificationCodeNotMatchedException;
 import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.UserStatus;
+import com.example.demo.model.dto.UserCreateDto;
+import com.example.demo.model.dto.UserUpdateDto;
 import com.example.demo.repository.UserEntity;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.jdbc.SqlGroup;
 
 @SpringBootTest
+@TestPropertySource("classpath:test-application.properties")
 @SqlGroup({
         @Sql(value = "/sql/user-service-test-data.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD),
         @Sql(value = "/sql/delete-all-data.sql", executionPhase = ExecutionPhase.AFTER_TEST_METHOD),
@@ -22,6 +33,9 @@ public class UserServiceTest {
     @Autowired
     private UserService userService;
 
+    @MockBean
+    private JavaMailSender mailSender;
+
     @Test
     void getByEmail은_ACTIVE_상태인_유저를_찾아올_수_잇다() {
 
@@ -29,10 +43,10 @@ public class UserServiceTest {
         String email = "dev.hyoseung@gmail.com";
 
         // when
-        UserEntity result = userService.getByEmail(email);
+        UserEntity userEntity = userService.getByEmail(email);
 
         // then
-        assertThat(result.getNickname()).isEqualTo("hyoseung");
+        assertThat(userEntity.getNickname()).isEqualTo("hyoseung");
     }
 
     @Test
@@ -44,7 +58,7 @@ public class UserServiceTest {
         // when
         // then
         assertThatThrownBy(() -> {
-            UserEntity result = userService.getByEmail(email);
+            UserEntity userEntity = userService.getByEmail(email);
         }).isInstanceOf(ResourceNotFoundException.class);
     }
 
@@ -53,10 +67,10 @@ public class UserServiceTest {
 
         // given
         // when
-        UserEntity result = userService.getById(1);
+        UserEntity userEntity = userService.getById(1);
 
         // then
-        assertThat(result.getNickname()).isEqualTo("hyoseung");
+        assertThat(userEntity.getNickname()).isEqualTo("hyoseung");
     }
 
     @Test
@@ -66,8 +80,83 @@ public class UserServiceTest {
         // when
         // then
         assertThatThrownBy(() -> {
-            UserEntity result = userService.getById(2);
+            UserEntity userEntity = userService.getById(2);
         }).isInstanceOf(ResourceNotFoundException.class);
+    }
+
+    @Test
+    void userCreateDto_를_이용하여_유저를_생성할_수_있다() {
+
+        // given
+        UserCreateDto userCreateDto = UserCreateDto.builder()
+                .email("ubin@naver.com")
+                .address("Suwon")
+                .nickname("ubin")
+                .build();
+        BDDMockito.doNothing().when(mailSender).send(any(SimpleMailMessage.class));
+
+        // when
+        UserEntity userEntity = userService.create(userCreateDto);
+
+        // then
+        assertThat(userEntity.getId()).isNotNull();
+        assertThat(userEntity.getStatus()).isEqualTo(UserStatus.PENDING);
+        // assertThat(userEntity.getCertificationCode()).isEqualTo("T.T");
+    }
+
+    @Test
+    void userUpdateDto_를_이용하여_유저를_수정할_수_있다() {
+
+        // given
+        UserUpdateDto userUpdateDto = UserUpdateDto.builder()
+                .address("Anyang")
+                .nickname("hyoseung-n")
+                .build();
+
+        // when
+        userService.update(1L, userUpdateDto);
+
+        // then
+        UserEntity userEntity = userService.getById(1);
+        assertThat(userEntity.getId()).isNotNull();
+        assertThat(userEntity.getAddress()).isEqualTo("Anyang");
+        assertThat(userEntity.getNickname()).isEqualTo("hyoseung-n");
+    }
+
+    @Test
+    void user를_로그인_시키면_마지막_로그인_시간이_변경된다() {
+
+        // given
+        // when
+        userService.login(1);
+
+        // then
+        UserEntity userEntity = userService.getById(1);
+        assertThat(userEntity.getLastLoginAt()).isGreaterThan(0L);
+        // assertThat(userEntity.getLastLoginAt()).isEqualTo("T.T");
+    }
+
+    @Test
+    void PENDING_상태의_사용자는_인증_코드로_ACTIVE_시킬_수_있다() {
+
+        // given
+        // when
+        userService.verifyEmail(2, "aaaaaaaa-aaaa-aaaa-aaaaaaaaaaab");
+
+        // then
+        UserEntity userEntity = userService.getById(2);
+        assertThat(userEntity.getStatus()).isEqualTo(UserStatus.ACTIVE);
+    }
+
+    @Test
+    void PENDING_상태의_사용자는_잘못된_인증_코드를_받으면_에러를_던진다() {
+
+        // given
+        // when
+        // then
+        assertThatThrownBy(() -> {
+            userService.verifyEmail(2, "aaaaaaaa-aaaa-aaaa-aaaaaaaaaaac");
+        }).isInstanceOf(CertificationCodeNotMatchedException.class);
     }
 
 }
--- src/test/resources/sql/user-service-test-data.sql
+++ src/test/resources/sql/user-service-test-data.sql
@@ -1,5 +1,6 @@
+
 insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
 values (1, 'dev.hyoseung@gmail.com', 'hyoseung', 'Suwon', 'aaaaaaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
 
 insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
-values (2, 'jaeyeon@gmail.com', 'jaeyeon', 'Suwon', 'aaaaaaaa-aaaa-aaaa-aaaaaaaaaaaa', 'PENDING', 0);
+values (2, 'jaeyeon@gmail.com', 'jaeyeon', 'Suwon', 'aaaaaaaa-aaaa-aaaa-aaaaaaaaaaab', 'PENDING', 0);
