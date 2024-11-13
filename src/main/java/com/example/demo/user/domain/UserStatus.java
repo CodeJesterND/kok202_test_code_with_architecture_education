e8f548cef43243a861083999fcb0df54b1c5c32f
CodeJesterND <dev.hyoseung@gmail.com>
2024-11-13 15:32:42 +0900

    ✅ 테스트 추가 : 테스트 패키지 구조 조정

---
+++ src/main/java/com/example/demo/user/domain/UserStatus.java
@@ -0,0 +1,11 @@
+package com.example.demo.user.domain;
+
+public enum UserStatus {
+
+    PENDING,
+
+    INACTIVE,
+
+    ACTIVE
+
+}
--- src/test/resources/sql/post-service-test-data.sql
+++
@@ -1,9 +0,0 @@
-
-insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
-values (1, 'dev.hyoseung@gmail.com', 'hyoseung', 'Suwon', 'aaaaaaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
-
-insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
-values (2, 'jaeyeon@gmail.com', 'jaeyeon', 'Suwon', 'aaaaaaaa-aaaa-aaaa-aaaaaaaaaaab', 'PENDING', 0);
-
-insert into `posts` (`id`, `content`, `created_at`, `modified_at`, `user_id`)
-values (1, 'hello world', 1_678_350_673_958, 0, 1);
